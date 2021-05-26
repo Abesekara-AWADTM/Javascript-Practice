2c709b533a0ad20191bff571cad0d83782133513
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2021-05-25 23:11:40 -0700

    variable

---
+++ Variables/variable.js
@@ -0,0 +1,35 @@
+
+
+
+// --Primitive Data Types--
+/* Number - Floating point numbers, for decimals and integers
+   String - Sequence of characters, used for text
+   Boolean - Logical data type that can only be true or false
+   Undefined - Data type of a variable that does not have a value yet
+   Null - Non-existent */
+
+   // You only can start a varible name with a letter, underscore or dollar sign.
+// And can't use any reserved keywords as variable names
+
+
+
+// Create variables
+var firstName = 'Thilini'; //use camal case
+var age =23;
+var isOk=true;
+var job, height;
+
+// Print only variable
+console.log(firstName);
+
+// Print the variable with a string
+console.log("My name is "+firstName);
+
+// Type Coercion
+console.log("My age is "+ age);
+console.log(" It is "+ isOk);
+console.log(" My Job is "+ job);
+
+
+
+
