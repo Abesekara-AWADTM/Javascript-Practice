18c0781af921d58d9dd6e1f5295332471b1c5db1
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2021-05-27 03:23:32 -0700

    Update assignmentOperators.js

--- Assignment Operators/assignmentOperators.js
+++ Assignment Operators/assignmentOperators.js
@@ -1,1 +1,32 @@
 // ------Assignment Operators------
+
+// Equal
+
+var myName='Thilini';
+console.log(myName);
+ 
+//addition with equal
+
+var age=23;
+age+=10;
+console.log(age);
+
+//substraction with equal
+age-=10;
+console.log(age);
+
+// Multiplication with equal
+age *= 10;
+console.log(age);
+
+// Division with equal
+age /= 4;
+console.log(age);
+
+// Modulus with equal
+age %= 13;
+console.log(age);
+
+// Exponentiation with equal
+age **= 2;
+console.log(age);
